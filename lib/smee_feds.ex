a43775a4fe0af8be42343e766370e69d912d6c4e
Pete Birkinshaw <pete@binary-ape.org>
2023-03-19 17:08:27 +0000

    First commit, basic data, no code

---
+++ lib/smee_feds.ex
@@ -0,0 +1,18 @@
+defmodule SmeeFeds do
+  @moduledoc """
+  Documentation for `SmeeFeds`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SmeeFeds.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/smee_feds_test.exs
@@ -0,0 +1,8 @@
+defmodule SmeeFedsTest do
+  use ExUnit.Case
+  doctest SmeeFeds
+
+  test "greets the world" do
+    assert SmeeFeds.hello() == :world
+  end
+end
