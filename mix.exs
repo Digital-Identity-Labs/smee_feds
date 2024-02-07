780097b5c16be7f21e35d8aca6f23fbdb9280065
Pete Birkinshaw <pete@binary-ape.org>
2024-02-07 11:39:00 +0000

    Update deps, use latest Smee (0.4.0)

--- mix.exs
+++ mix.exs
@@ -4,9 +4,9 @@ defmodule SmeeFeds.MixProject do
   def project do
     [
       app: :smee_feds,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.14",
-      description: "A federation extension for Smee",
+      description: "A federation extension for Smee, with example data",
       package: package(),
       name: "SmeeFeds",
       source_url: "https://github.com/Digital-Identity-Labs/smee_feds",
@@ -43,20 +43,20 @@ defmodule SmeeFeds.MixProject do
   defp deps do
     [
       {:countries, "~> 1.6"},
-      {:smee, ">= 0.0.0"},
+      {:smee, "~> 0.4"},
       {:jason, "~> 1.4"},
-      {:csv, "~> 3.0"},
+      {:csv, "~> 3.2"},
       {:rambo, "~> 0.3.4"},     # temporary fix
       {:ex_json_schema, "~> 0.9.2"},
 
       {:apex, "~> 1.2", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.6.7", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.14 and >= 0.14.4", only: [:dev, :test]},
-      {:benchee, "~> 1.0.1", only: [:dev, :test]},
-      {:ex_doc, "~> 0.23.0", only: :dev, runtime: false},
-      {:earmark, "~> 1.3", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
-      {:doctor, "~> 0.17.0", only: :dev, runtime: false}
+      {:benchee, "~> 1.3", only: [:dev, :test]},
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false},
+      {:earmark, "~> 1.4", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
+      {:doctor, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 
