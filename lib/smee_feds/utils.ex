20c5a31d95210f6974b7d439760c27fb532e709f
Pete Birkinshaw <pete@binary-ape.org>
2024-02-17 16:12:53 +0000

    We need a Utils module here too

---
+++ lib/smee_feds/utils.ex
@@ -0,0 +1,26 @@
+defmodule SmeeFeds.Utils do
+  @moduledoc false
+
+  def to_safe_atoms(input) do
+    input
+    |> List.wrap()
+    |> Enum.map(fn x -> to_safe_atom(x) end)
+    |> Enum.reject(fn x -> is_nil(x) end)
+  end
+
+  def to_safe_atom(input) when is_binary(input) do
+    try do
+      input
+      |> String.trim()
+      |> String.downcase()
+      |> String.to_existing_atom()
+    rescue
+      _ -> nil
+    end
+  end
+
+  def to_safe_atom(input) when is_atom(input) do
+    input
+  end
+
+end
